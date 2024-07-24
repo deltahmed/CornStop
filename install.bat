af64628c28660d07882466c15a679ce55ba97212
fantasijump <108099988+fantasijump@users.noreply.github.com>
2024-07-24 22:57:23 +0200

    install update

--- install.bat
+++ install.bat
@@ -1,17 +1,15 @@
 @echo off
 
-set "batch_path=%~dp0"
+set "batch_path=%~dp0" 
 set "program_name=shut_p.exe"
 set "startup_folder=%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
 set "program_path=%startup_folder%\%program_name%"
 
 if exist "%program_path%" (
-    del %program_path%
+    move "%startup_folder%\%program_name%" "%tmp%"
     echo ancien programme delete reussi
 )
-
 move "%batch_path%%program_name%" "%startup_folder%\%program_name%"
-
 
 if exist "%program_path%" (
     echo Installation reussie.
